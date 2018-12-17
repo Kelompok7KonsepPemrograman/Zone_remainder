1db0db5a674028f6413fb242c41575fb156bf9fe
mamengko <45219093+mamengko@users.noreply.github.com>
2018-12-17 14:38:49 +0700

    Add files via upload

---
+++ jam4.py
@@ -0,0 +1,17 @@
+from set 1 import*
+
+class jam:
+	def komponen(self):
+		self.texsjam = stringvar()
+		self.teks =label(teks="KELOMPOK7")
+		self.teks.pack()
+		
+		layarjam = frame(self.parent,bd=10)
+		layarjam.pack()
+		
+		self.jam = label(layar jam, texvariable=selg.teksjam,font=("hekvetica",40,'bold),bg ="light blue",fg="blue)
+		self.jam.pack()
+		
+	from set 3 import*
+		
+	
